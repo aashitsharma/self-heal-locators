84ff0157794bf0d58d6b0b1a43eee4e97705fd29
Aashit Sharma <aashit.sharma@1mg.com>
2025-01-15 20:50:47 +0530

    Mongo DB

--- src/main/java/onemg/analytics/dump/ConfigController.java
+++ src/main/java/onemg/analytics/dump/ConfigController.java
@@ -1,13 +1,18 @@
 package onemg.analytics.dump;
 
+import onemg.analytics.dump.controller.AnalyticsDataController;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.FileReader;
 import java.util.Properties;
 
 @RestController
 public class ConfigController {
-
+    private static final Logger LOGGER = Logger.getLogger(ConfigController.class);
     @Autowired
     private ConfigProperties configProperties;
 
@@ -15,8 +20,15 @@ public class ConfigController {
     public ConfigProperties getConfig() {
         Properties pr = new Properties();
         try{
-            pr.load(ConfigController.class.getClassLoader().getResourceAsStream("src/main/resources/application.properties"));
+            File reader = new File("src/main/resources/application.properties");
+            if (reader.exists()) {
+                pr.load(new FileReader(reader));
+            }
+            if (!reader.exists()) {
+                pr.load(ConfigController.class.getResourceAsStream("/application.properties"));
+            }
             pr.setProperty("spring.data.mongodb.uri",configProperties.getSpringDataMongodbUri());
+            LOGGER.info("Fetching Property Details : "+pr.getProperty("spring.data.mongodb.uri"));
         }
         catch (Exception e){
             e.printStackTrace();
