fd3836313c2c3593c720a152fef7422bebd51916
Aashit Sharma <aashit.sharma@1mg.com>
2025-01-22 09:50:56 +0530

    changed in main file

--- src/main/java/onemg/analytics/dump/AnalyticsDataDumpApplication.java
+++ src/main/java/onemg/analytics/dump/AnalyticsDataDumpApplication.java
@@ -6,11 +6,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class AnalyticsDataDumpApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AnalyticsDataDumpApplication.class, args);
+    public static void main(String[] args) throws IOException {
+        SpringApplication app = new SpringApplication(AnalyticsDataDumpApplication.class);
+        JsonConfig js = new JsonConfig();
+        js.configProperties();
+        app.setDefaultProperties(JsonConfig.props);
+        app.run(args);
     }
 
 }
--- src/main/java/onemg/analytics/dump/JsonConfig.java
+++ src/main/java/onemg/analytics/dump/JsonConfig.java
@@ -8,24 +8,27 @@ import org.springframework.context.annotation.Configuration;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 @Configuration
 public class JsonConfig {
     private static final Logger LOGGER = Logger.getLogger(JsonConfig.class);
-
+    public static Map<String, Object> props = new HashMap<>();
     @Bean
     public ConfigProperties configProperties() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         LOGGER.info("Attempting to load config.json...");
         // Path to the root-level config.json file
         File file = new File("config.json");
-        if (!file.exists()) {
-            throw new IOException("config.json not found at root level: " + file.getAbsolutePath());
-        }
-
         ConfigProperties config = objectMapper.readValue(file, ConfigProperties.class);
         LOGGER.info("Loaded ConfigProperties: "+ config);
+        setProperties(config);
+        return config;
+    }
+
+    public static void setProperties(ConfigProperties config){
         System.setProperty("spring.data.mongodb.uri",config.getSpringDataMongodbUri());
         /*System.setProperty("logging.level.root",config.getLoggingLevel());
         System.setProperty("logging.level.org.springframework",config.getLoggingSpring());
@@ -34,6 +37,14 @@ public class JsonConfig {
         System.setProperty("logging.level.root","DEBUG");
         System.setProperty("logging.level.org.springframework","DEBUG");
         System.setProperty("server.port","8980");
-        return config;
+
+        props.put("spring.data.mongodb.uri",config.getSpringDataMongodbUri());
+        /*props.put("logging.level.root",config.getLoggingLevel());
+        props.put("logging.level.org.springframework",config.getLoggingSpring());
+        props.put("server.port",String.valueOf(config.getServerPort()));*/
+
+        props.put("logging.level.root","DEBUG");
+        props.put("logging.level.org.springframework","DEBUG");
+        props.put("server.port","8980");
     }
 }
